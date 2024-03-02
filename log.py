4a830cf75a8faa2ac2ffc8b047dd5a6755536c32
DoyunShin <doyun.shin@gmail.com>
2024-03-02 20:20:47 +0900

    feat: Log

---
+++ log.py
@@ -0,0 +1,34 @@
+import logging
+from prompt_toolkit import print_formatted_text, ANSI
+
+logger = None
+
+class PrintHandler(logging.Handler):
+    def emit(self, record):
+        print_formatted_text(ANSI(self.format(record)))
+
+def setup(name: str, debug: bool = False):
+    """
+    Setup a logger with the given name.
+    """
+    global logger
+    logger = logging.getLogger(name)
+    if debug:
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
+
+    # create console handler and set level to debug
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+
+    # create formatter
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    )
+
+    # add formatter to ch
+    ch.setFormatter(formatter)
+
+    # add ch to logger
+    logger.addHandler(ch)
