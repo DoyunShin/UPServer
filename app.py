e694abce4d3faee44fc68d6f9ad3eb93a4542eed
DoyunShin <doyun.shin@gmail.com>
2024-04-19 18:00:45 +0900

    fix: Too many fixes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, send_from_directory, abort, Response, redirect
+from flask import Flask, request, send_from_directory, abort, Response, redirect, jsonify
 from flask_cors import CORS
 import werkzeug
 from pathlib import Path
@@ -10,26 +10,39 @@ BASE_DIR = Path(__file__).resolve().parent
 STATIC_DIR = BASE_DIR / "static"
 ASSETS_DIR = STATIC_DIR / "assets"
 
-config = loads((BASE_DIR / "config.json").read_text())
-if config["storage"] not in filesystem.storageTypes: 
-    raise ValueError(f"Invalid storage type: {config['storage']}")
-storage: filesystem.storage = getattr(filesystem, config["storage"])(config)
+configPath: Path = None
+config: dict = None
+storage: filesystem.storage = None
 cache: dict[str, dict] = {}
 # fileid: {time: int, metadata: dict}
 
 app = Flask(__name__)
 CORS(app)
 
-if config["host"]["proxy"]:
-    from werkzeug.middleware.proxy_fix import ProxyFix
-    app.wsgi_app = ProxyFix(
-        app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
-    )
+def load_config():
+    global storage, app, config, configPath
+    if not configPath:
+        configPath = BASE_DIR / "config.json"
 
-def store_cache(fileid: str, metadata: dict):
-    cache[fileid] = {"time": int(time.time()), "metadata": metadata}
+    if not configPath.exists():
+        print("Config file not found!")
+        exit(-1)
 
-def get_cache(fileid: str) -> dict:
+    config = loads(configPath.read_text())
+    if config["storage"] not in filesystem.storageTypes: 
+        raise ValueError(f"Invalid storage type: {config['storage']}")
+    storage = getattr(filesystem, config["storage"])(config, configPath)
+
+    if config["host"]["proxy"]:
+        from werkzeug.middleware.proxy_fix import ProxyFix
+        app.wsgi_app = ProxyFix(
+            app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+        )
+
+def store_cache(metadata: filesystem.Metadata):
+    cache[metadata.id] = {"time": int(time.time()), "metadata": metadata}
+
+def get_cache(fileid: str) -> filesystem.Metadata:
     if fileid in cache:
         if cache[fileid]["time"] + config["host"]["cachetime"] > int(time.time()):
             return cache[fileid]["metadata"]
@@ -42,13 +55,13 @@ def clear_cache():
         if cache[fileid]["time"] + config["host"]["cachetime"] < int(time.time()):
             cache.pop(fileid)
 
-def load_metadata(fileid: str, filename: str) -> dict:
+def load_metadata(fileid: str, filename: str):
     metadata = get_cache(fileid)
     if metadata is None:
         metadata = storage.load_metadata(fileid, filename)
-        metadata.pop("folderid")
-        metadata.pop("delete")
-        store_cache(fileid, metadata)
+        metadata.folderid = None
+        metadata.delete = None
+        store_cache(metadata)
     return metadata
 
 @app.route('/info')
@@ -71,8 +84,8 @@ def v1api(path):
     filename = dt[1]
 
     try:
-        data = load_metadata(fileid, filename)
-        return data, 200
+        data = load_metadata(fileid, filename).to_dict()
+        return jsonify(data), 200
     except FileNotFoundError:
         return abort(404)
 
@@ -103,15 +116,19 @@ def get(path):
 
 @app.route('/get/<path:path>', methods=['GET'])
 def getf(path):
+    if path.startswith("delete/"): return abort(404)
     dt = path.split("/")
     if len(dt) > 2: return abort(404)
     fileid = dt[0]
     filename = dt[1]
     try:
-        redirect_url = config['host']['cdn']['url']
-        redirect_url = redirect_url[:-1] if redirect_url[-1] == "/" else redirect_url
-        if config["host"]["cdn"]["enabled"]: return redirect(f"{redirect_url}/{fileid}/{filename}")
+        if config["host"]["cdn"]["enabled"]: 
+            redirect_url = config['host']['cdn']['url']
+            redirect_url = redirect_url[:-1] if redirect_url[-1] == "/" else redirect_url
+            return redirect(f"{redirect_url}/{fileid}/{filename}")
+        
         return storage.download(fileid, filename)
+    
     except FileNotFoundError:
         return abort(404)
 
@@ -119,12 +136,15 @@ def getf(path):
 def putf(path):
     if "/" in path: return "Invalid path", 400
     if path[0] == ".": return "Invalid path", 400
-    
-    metadata = storage.save(request.stream, request.content_length, path)
-    store_cache(metadata["id"], metadata)
-    domain = config["host"]["domain"] if config["host"]["domain"] else request.host_url
 
-    return f"{domain}{metadata['id']}/{metadata['name']}", 200
+    metadata = storage.save(request.stream, request.content_length, path)
+    store_cache(metadata)
+
+    unknown_domains = ["localhost", "127.0.0.1"]
+    if request.host_url.split("//")[1].split("/")[0] in unknown_domains:
+        print("WARNING: Host URL is not set correctly! Check your proxy settings. (HOST Header)")
+
+    return f"{request.host_url}{metadata.id}/{metadata.name}", 200
 
 @app.errorhandler(404)
 def E404(e):
@@ -144,5 +164,6 @@ def error(code: str, msg: str):
     return STATIC_DIR.joinpath("error.html").read_text().replace("StatusCode", code).replace("StatusMessage", msg), int(code)
 
 if __name__ == '__main__':
+    load_config()
     app.debug = config["debug"]
     app.run(host=config["host"]["ip"], threaded=True, port=config["host"]["port"])
