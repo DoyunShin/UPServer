3dd266572759504ed719281264ffa5afe31e7f5d
DoyunShin <doyun.shin@gmail.com>
2026-04-25 14:33:52 +0900

    remove: Re-design frontend

--- frontend/src/routes/[...path]/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    import DownloadView from "$lib/DownloadView.svelte";
-    import { page } from "$app/stores";
-
-    const [fileid, ...filenameParts] = $page.params.path.split("/");
-    const filename = filenameParts.join("/");
-</script>
-
-<DownloadView fileid={fileid} filename={filename} />
